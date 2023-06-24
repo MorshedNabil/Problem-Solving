cb3394aad9cc2df0ad4b15567597532b798a896b
MorshedNabil <nabilmorshed1251@gmail.com>
2023-06-24 23:31:35 +0600

    code force problems

---
+++ Word_capitalize.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string s;
+    cin >> s;
+
+    if(s[0] > 97 && s[0] < 122)
+    {
+        s[0] = s[0] - 32; /// by subtracting 32 from the ascii val of first char we will get the 
+                        ////  uppercase of the char(if the char not in uppercase)
+    }
+    cout << s << "\n";
+
+    return 0;
+}
